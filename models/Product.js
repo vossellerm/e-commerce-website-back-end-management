65df3d0b89d7fbbe23759283729f1ebc610e5a25
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-06 14:52:00 -0700

    Products model

--- models/Product.js
+++ models/Product.js
@@ -11,10 +11,47 @@ Product.init(
   {
     // define columns
     //  id: integar, not null, primary key, auto increment
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
     // product name: string, not null
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
     // price: decimal, not null, validates decimal
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+
     // stock: integar, not null, default 10, validates number
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+
     // category id: integar, references Category model id
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
