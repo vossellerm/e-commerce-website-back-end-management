4928e0500a680f77b9c3097397b832e63dd32b01
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-06 15:35:49 -0700

    update tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -45,6 +45,13 @@ router.post("/", (req, res) => {
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
 
 router.delete("/:id", (req, res) => {
