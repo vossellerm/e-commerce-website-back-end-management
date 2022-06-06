9d7947ff571c00a6306b94c52eb31a9f499c1fee
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-06 14:58:20 -0700

    Tag model psuedocode

--- models/Tag.js
+++ models/Tag.js
@@ -1,19 +1,21 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
 Tag.init(
   {
     // define columns
+    // id: integar, not null, primary key, auto increment
+    // tag name: string
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
