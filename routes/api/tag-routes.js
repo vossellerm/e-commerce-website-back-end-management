6486542b7165a20750a27ed08a4dc09cdad4aa85
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-06 15:34:32 -0700

    create tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,6 +38,9 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
 
 router.put("/:id", (req, res) => {
