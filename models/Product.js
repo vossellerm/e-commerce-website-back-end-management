dcac932c65f7b3b6980c524019aaba4998a07e07
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-06 14:50:33 -0700

    Products model psuedocode

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,7 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,13 +10,18 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    //  id: integar, not null, primary key, auto increment
+    // product name: string, not null
+    // price: decimal, not null, validates decimal
+    // stock: integar, not null, default 10, validates number
+    // category id: integar, references Category model id
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
