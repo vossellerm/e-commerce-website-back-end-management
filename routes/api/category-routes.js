fe7c291d69669e344ab9a7bfdf01fb00e10887a2
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-06 15:40:07 -0700

    find one category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,6 +16,14 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product],
+  })
+    .then((category) => res.json(category))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.post("/", (req, res) => {
