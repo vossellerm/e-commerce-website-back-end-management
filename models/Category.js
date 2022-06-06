37f5f5230b1352bbfd1a57a7ec08b917bbde1809
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-06 14:45:39 -0700

    Category model psuedocode

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,21 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
     // define columns
+    // id: integar, not null, primary key, auto increment
+    // category name: string, not null
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
