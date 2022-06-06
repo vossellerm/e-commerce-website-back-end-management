2e00ac783902b53568a015c36b74ef333fc5e470
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-06 15:42:55 -0700

    delete category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,6 +46,13 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((category) => res.status(200).json(category))
+    .catch((err) => res.status(400).json(err));
 });
 
 module.exports = router;
