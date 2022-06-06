d7321a9b440606ee92831d905b2d3993c4dc013d
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-06 15:28:44 -0700

    product-routes delete product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -122,6 +122,19 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((products) => {
+      console.log(products);
+      req.json(products);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 module.exports = router;
