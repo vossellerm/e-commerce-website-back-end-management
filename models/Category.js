f19e1b527db86a7f98eff93182e2d90b308dd58a
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-06 14:47:43 -0700

    Category model

--- models/Category.js
+++ models/Category.js
@@ -8,7 +8,17 @@ Category.init(
   {
     // define columns
     // id: integar, not null, primary key, auto increment
+    id: {
+      type: DataTypes.INTEGAR,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     // category name: string, not null
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
