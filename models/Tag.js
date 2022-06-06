80a2c35c5d35b281dda436f02bf71c8f889f9ffa
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-06 14:59:56 -0700

    Tag model

--- models/Tag.js
+++ models/Tag.js
@@ -8,7 +8,16 @@ Tag.init(
   {
     // define columns
     // id: integar, not null, primary key, auto increment
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     // tag name: string
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
