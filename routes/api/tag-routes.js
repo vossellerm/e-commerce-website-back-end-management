c4cef0073f974c7a2ba03a747c3b32daecf59885
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-06 15:33:26 -0700

    find single tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -21,6 +21,19 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
+      },
+    ],
+  })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
 
 router.post("/", (req, res) => {
