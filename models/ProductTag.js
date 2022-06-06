60c38e2181a841b6611d387834e20989237a681d
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-06 14:54:45 -0700

    ProductTag model psuedocode

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,22 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    // id: integar, not null, primary key, auto increment
+    // product id: integar, reference Product model id
+    // tag id: integar, reference Tag model id
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
